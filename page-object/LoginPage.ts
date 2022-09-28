36d048243b128e91e4498d23076652aa58be0452
QuocBaoNguyen11295 <nqb11295@gmail.com>
2022-09-28 22:17:42 +0700

    Add login page object to POM

---
+++ page-object/LoginPage.ts
@@ -0,0 +1,38 @@
+import {expect,Locator,Page} from '@playwright/test'
+
+export class LoginPage{
+    readonly page: Page
+    readonly usernameInput: Locator
+    readonly passwordInput: Locator
+    readonly submitButton: Locator
+    readonly errorMessage: Locator
+    readonly signinButton: Locator
+    constructor(page: Page){
+        this.page = page
+        this.usernameInput = page.locator('#user_login')
+        this.passwordInput = page.locator('#user_password')
+        this.submitButton = page.locator('input[name="submit"]')
+        this.errorMessage = page.locator('.alert-error')
+        this.signinButton = page.locator('#signin_button')
+    }
+
+    async visit(){
+        await this.page.goto('http://zero.webappsecurity.com/index.html')
+        await this.signinButton.click()
+    }
+
+    async login(username: string,password: string){
+        await this.usernameInput.type(username.trim())
+        await this.passwordInput.type(password.trim())
+        await this.submitButton.click()
+    }
+    
+    async assertErrorMessage(){
+        await expect(this.errorMessage).toBeVisible()
+        await expect(this.errorMessage).toContainText('Login and/or password are wrong.')
+    }
+
+    async assertNoErrorMessage(){
+        await expect(this.errorMessage).not.toBeVisible()
+    }
+}
