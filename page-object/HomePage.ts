6ab11bba12a70c1b0ce56e440e37749f3891bb97
QuocBaoNguyen11295 <nqb11295@gmail.com>
2022-09-29 00:39:19 +0700

    Add search text object

--- page-object/HomePage.ts
+++ page-object/HomePage.ts
@@ -3,9 +3,11 @@ import {expect,Locator,Page} from '@playwright/test'
 export class HomePage{
     readonly page:Page
     readonly signinButton: Locator
+    readonly searchText: Locator
     constructor(page:Page){
         this.page = page
         this.signinButton = page.locator('#signin_button')
+        this.searchText = page.locator('#searchTerm')
     }
 
     async visitHomePage(){
@@ -16,7 +18,9 @@ export class HomePage{
         await this.signinButton.click()
     }
 
+    async typeKeywordToSearchBox(keyword: string){
+        await this.searchText.type(keyword.trim())
+        await this.page.keyboard.press('Enter')
+    }
 
-
-    
 }
